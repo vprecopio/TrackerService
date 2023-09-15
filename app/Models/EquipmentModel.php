c0077db88bb7676fb043246f43f34fa6a8e1d893
RoeZX <spsandoval94@gmail.com>
2023-09-15 20:46:16 -0300

    refactor:eliminar funciones innecesarias

--- app/Models/EquipmentModel.php
+++ app/Models/EquipmentModel.php
@@ -8,14 +8,9 @@ class EquipmentModel
 
     //cambiar en la base de datos por nombres singulares
     private $id_modelos_equipos;
-    private $id_marca;
-    private $marca_descripcion;
-
-    private $id_modelo;
-    private $modelo_equipo_descripcion;
-
-    private $id_categoria_equipo;
-    private $categoria_equipo_descripcion;
+    private $id_marca, $marca_descripcion;
+    private $id_modelo, $modelo_equipo_descripcion;
+    private $id_categoria_equipo, $categoria_equipo_descripcion;
 
     public function __construct()
     {
@@ -33,7 +28,6 @@ class EquipmentModel
             die($e->getMessage());
         }
     }
-
     public function CreateEquipmentBrand()
     {
         $sql = "INSERT INTO `equipos_marca` (`marca_descripcion`) VALUES (:marca_descripcion)";
@@ -46,17 +40,6 @@ class EquipmentModel
         return $stm->execute($params);
     }
 
-    public function ListEquipmentModels()
-    {
-        try {
-            $stm = $this->pdo->prepare("SELECT * FROM equipos_modelo");
-
-            $stm->execute();
-            return $stm->fetchAll(\PDO::FETCH_OBJ);
-        } catch (\Exception $e) {
-            die($e->getMessage());
-        }
-    }
 
     public function ListEquipmentCategories()
     {
@@ -69,7 +52,6 @@ class EquipmentModel
             die($e->getMessage());
         }
     }
-
     public function CreateEquipmentCategories()
     {
         $sql = "INSERT INTO `categorias_equipos` (`categoria_equipo_descripcion`) VALUES (:categoria_equipo_descripcion)";
@@ -82,22 +64,26 @@ class EquipmentModel
         return $stm->execute($params);
     }
 
+
+
     public function ListEquipments()
     {
         try {
-            $stm = $this->pdo->prepare("SELECT
-          modelos_equipos.id_modelos_equipos as equipo,
-          equipos_marca.marca_descripcion as marca,
-          equipos_modelo.descripcion as modelo,
-          categorias_equipos.categoria_equipo_descripcion as categoria
-        FROM
-          modelos_equipos
-        JOIN
-          equipos_marca ON modelos_equipos.id_marca = equipos_marca.id_marcas
-        JOIN
-          equipos_modelo ON modelos_equipos.id_modelo = equipos_modelo.id_modelo
-        JOIN
-          categorias_equipos ON modelos_equipos.id_categoria_equipo = categorias_equipos.id_categoria_equipo");
+            $stm = $this->pdo->prepare(
+                "SELECT
+                modelos_equipos.id_modelos_equipos as equipo,
+                equipos_marca.marca_descripcion as marca,
+                equipos_modelo.descripcion as modelo,
+                categorias_equipos.categoria_equipo_descripcion as categoria
+                FROM
+                modelos_equipos
+                JOIN
+                equipos_marca ON modelos_equipos.id_marca = equipos_marca.id_marcas
+                JOIN
+                equipos_modelo ON modelos_equipos.id_modelo = equipos_modelo.id_modelo
+                JOIN
+                categorias_equipos ON modelos_equipos.id_categoria_equipo = categorias_equipos.id_categoria_equipo"
+            );
 
             $stm->execute();
             return $stm->fetchAll(\PDO::FETCH_OBJ);
