b87ddbfd2343e22ad3e58ccc91fc4052686c1304
onunezz <alegusnunez@gmail.com>
2023-09-17 19:34:02 -0300

    feat: Formulario de contraseña olvidada
    Creación del formulario de contraseña perdida y la conexión en el login hacia el mismo.

--- views/authforgot.php
+++ views/authforgot.php
@@ -1,19 +1,78 @@
-<div class="flex flex-col items-center justify-center min-h-screen p-4 space-y-4 antialiased text-gray-900 bg-gray-100">
-    <a href="/"><img class="h-[60px] w-[60px] mb-6" src="/public/src/img/logo.png" alt="logo" /></a>
-    <main>
-      <div class="bg-white shadow-xl p-10 flex flex-col gap-6 text-sm w-96">
-        <p class="text-sm font-medium text-center text-gray-500">
-          Si olvidaste/perdiste tu contraseña podés pedirnos una solicitud de recuperación ingresando tu correo electrónico asociado
-        </p>
-        <div class="mb-2">
-          <input class="border border-gray-400 focus:outline-[#06b6d4] rounded-md w-full shadow-sm px-5 py-2" type="email" name="email" placeholder="Correo electrónico" />
+<body>
+    <nav class="fixed z-30 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+        <div class="px-3 py-3 lg:px-5 lg:pl-3">
+            <div class="flex items-center justify-between">
+                <div class="flex items-center justify-start">
+                    <a href="/" class="flex ml-2 md:mr-24">
+                        <img src="../public/src/img/logo.png" class="h-8 mr-3" alt="TecnoGestion Logo">
+                        <span class="fuente self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Tecno Gestión</span>
+                    </a>
+                </div>
+
+                <div class="flex items-center">
+                    <div class="hidden mr-3 -mb-1 sm:block">
+                        <span></span>
+                    </div>
+
+                    <script async="" defer="" src="https://buttons.github.io/buttons.js"></script>
+
+                    <!-- Profile -->
+                    <div class="flex items-center ml-3">
+                        <div>
+                            <button type="button" class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button-2" aria-expanded="false" data-dropdown-toggle="dropdown-2">
+                                <span class="sr-only">Open user menu</span>
+                                <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="user photo">
+                            </button>
+                        </div>
+                        <!-- Dropdown menu -->
+                        <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-2" style="position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate(1263px, 61px);" data-popper-placement="bottom">
+                            <div class="px-4 py-3" role="none">
+                                <p class="text-sm text-gray-900 dark:text-white" role="none">
+                                    Neil Sims
+                                </p>
+                                <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
+                                    neil.sims@flowbite.com
+                                </p>
+                            </div>
+                            <ul class="py-1" role="none">
+                                <li>
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Dashboard</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Settings</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Earnings</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Sign out</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div>
-          <input class="bg-[#4fd1c5] w-full py-2 mt-4 rounded-md text-white font-bold cursor-pointer hover:bg-[#34a49a]" type="submit" value="Solicitar" />
+    </nav>
+
+    <div id="main-content" class="relative flex items-center justify-center flex-wrap flex-col w-full h-full overflow-y-auto bg-gray-50 dark:bg-gray-900 min-h-screen">
+        <div class="flex justify-center">
+            <a href="#"><img class="h-[60px] w-[60px] mb-6" src="/public/src/img/logo.png" alt="logo" /></a>    
         </div>
-        <div class="text-sm text-gray-600 dark:text-gray-400">
-          <a href="/auth/" class="text-blue-600 hover:underline">Cancelar</a>
+    <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <form class="space-y-6" action="#" method="POST">
+                <p class="text-sm font-medium text-center text-white">Si olvidaste/perdiste tu contraseña podés pedirnos una solicitud de recuperación ingresando tu correo electrónico asociado</p>
+                <div>
+                    <label for="email" class="block pl-2.5 mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                    <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="nombre@compañía.com" required>
+                </div>
+                <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Solicitar</button>
+                <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+                    <a href="/auth/login/" class="text-blue-700 hover:underline dark:text-blue-500">Cancelar</a>
+                </div>
+            </form>
         </div>
-      </div>
-    </main>
-</div>
+
+    </div>
+
+</body>
--- views/layout.php
+++ views/layout.php
@@ -416,7 +416,7 @@ $esta_logueado = 0;
 
 
     <?php else : ?>
-        <?php require __DIR__ . '/authregister.php'; ?>
+        <?php require __DIR__ . '/authforgot.php'; ?>
     <?php endif; ?>
 </body>
 
