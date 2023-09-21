a6141726d1312e7a2e324e807cab8a91f2c23ee0
RoeZX <spsandoval94@gmail.com>
2023-09-21 18:15:07 -0300

    fix: funcion delete

--- views/client.php
+++ views/client.php
@@ -139,7 +139,7 @@ $client_controller = new ClientController;
                                                     Editar cliente
                                                 </button>
 
-                                                <button type="button" id="deleteClientButton" data-client-id="<?php echo $obj_client->id_cliente; ?>" data-drawer-target="drawer-delete-client-default" data-drawer-show="drawer-delete-client-default" aria-controls="drawer-delete-client-default" data-drawer-placement="right" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
+                                                <button type="button" id="deleteClientButton" onclick="handleDeleteButtonClick('<?php echo $obj_client->id_cliente?>')" data-drawer-target="drawer-delete-client-default" data-drawer-show="drawer-delete-client-default" aria-controls="drawer-delete-client-default" data-drawer-placement="right" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
                                                     <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                         <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
                                                     </svg>
@@ -277,19 +277,12 @@ $client_controller = new ClientController;
     </div>
 </div>
 
-
 <script>
-    //script eliminar cliente button
-    document.addEventListener("DOMContentLoaded", function() {
+    function handleDeleteButtonClick(clientId) {
         var deleteButton = document.getElementById("deleteClientButton");
-        var clientId = deleteButton.getAttribute("data-client-id");
         var deleteLink = document.querySelector("#drawer-delete-client-default a.text-white.bg-red-600");
-
-        deleteButton.addEventListener("click", function() {
-            // Establecer la ID del cliente en el enlace de eliminación
-            deleteLink.href = "/client/delete/?Documento=" + clientId;
-        });
-    });
+        deleteLink.href = "/client/delete/?Documento=" + clientId;
+    }
 </script>
 
 <script>
