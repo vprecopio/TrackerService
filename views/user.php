52d8d4775edbdf0b7a0af45e4716bf3f2728ea77
onunezz <alegusnunez@gmail.com>
2023-09-23 21:30:14 -0300

    fix: Correcciones formularios clientes y usuarios
    Se arreglaron los estilos en ambos formularios

--- views/user.php
+++ views/user.php
@@ -13,7 +13,7 @@ $user_controller = new UserController;
                         <nav class="flex mb-5" aria-label="Breadcrumb">
                             <ol class="inline-flex items-center space-x-1 text-sm font-medium md:space-x-2">
                                 <li class="inline-flex items-center">
-                                    <a href="#" class="inline-flex items-center text-gray-700 hover:text-primary-600 dark:text-gray-300 dark:hover:text-white">
+                                    <a href="/" class="inline-flex items-center text-gray-700 hover:text-primary-600 dark:text-gray-300 dark:hover:text-white">
                                         <svg class="w-5 h-5 mr-2.5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
                                         </svg>
@@ -25,7 +25,7 @@ $user_controller = new UserController;
                                         <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
                                         </svg>
-                                        <a href="#" class="ml-1 text-gray-700 hover:text-primary-600 md:ml-2 dark:text-gray-300 dark:hover:text-white">Users</a>
+                                        <a href="/crud/" class="ml-1 text-gray-700 hover:text-primary-600 md:ml-2 dark:text-gray-300 dark:hover:text-white">Crud</a>
                                     </div>
                                 </li>
                                 <li>
@@ -33,7 +33,7 @@ $user_controller = new UserController;
                                         <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
                                         </svg>
-                                        <span class="ml-1 text-gray-400 md:ml-2 dark:text-gray-500" aria-current="page">List</span>
+                                        <span class="ml-1 text-gray-400 md:ml-2 dark:text-gray-500" aria-current="page">Usuarios</span>
                                     </div>
                                 </li>
                             </ol>
@@ -51,6 +51,15 @@ $user_controller = new UserController;
                                 </div>
                             </form>
                         </div>
+                        <!--Fin buscar-->
+                        <div class="flex items-center ml-auto space-x-2 sm:space-x-3">
+                            <a type="button" href="/user/" type="button" data-refresh="" class="inline-flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-sm px-3 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                                <svg class="w-5 h-5 mr-2 -ml-1" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"></path>
+                                </svg>
+                                Refrescar
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -58,81 +67,75 @@ $user_controller = new UserController;
                 <div class="overflow-x-auto">
                     <div class="inline-block min-w-full align-middle">
                         <div class="overflow-hidden shadow">
-                            <table class="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-600">
-                                <!--Titulos-->
-                                <thead class="bg-gray-100 dark:bg-gray-700">
-                                    <tr>
-                                        <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                                            Nombre
-                                        </th>
-                                        <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                                            Apellido
-                                        </th>
-                                        <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                                            Email
-                                        </th>
-                                        <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                                            Estado
-                                        </th>
-                                        <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                                            Rol
-                                        </th>
-                                        <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
-                                            Acciones
-                                        </th>
-                                    </tr>
-                                </thead>
+                        <table class="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-600">
+    <!--Titulos-->
+    <thead class="bg-gray-100 dark:bg-gray-700">
+        <tr>
+            <th scope="col" class="p-4 text-xs font-bold text-left text-gray-500 uppercase dark:text-gray-400 w-1/5">
+                Nombre completo
+            </th>
+            <th scope="col" class="p-4 text-xs font-bold text-left text-gray-500 uppercase dark:text-gray-400 w-1/5">
+                Email
+            </th>
+            <th scope="col" class="p-4 text-xs font-bold text-left text-gray-500 uppercase dark:text-gray-400 w-1/5">
+                Estado
+            </th>
+            <th scope="col" class="p-4 text-xs font-bold text-left text-gray-500 uppercase dark:text-gray-400 w-1/5">
+                Rol
+            </th>
+            <th scope="col" class="p-4 text-xs font-bold text-left text-gray-500 uppercase dark:text-gray-400 w-1/5">
+                Acciones
+            </th>
+        </tr>
+    </thead>
 
-                                <!--Usuarios-->
-                                <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
-                                    <?php foreach ($user_controller->ListUser() as $obj_user) : ?>
-                                        <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
+    <!--Usuarios-->
+    <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+        <?php foreach ($user_controller->ListUser() as $obj_user) : ?>
+            <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
+                <!--datos-->
+                <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white w-1/5">
+                    <data value="nombre"><?= $obj_user->usr_nombre . ' ' . $obj_user->usr_apellido ?></data>
+                </td>
 
-                                            <!--datos-->
-                                            <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                <data value="nombre"><?= $obj_user->usr_nombre ?></data>
-                                            </td>
+                <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white w-1/5">
+                    <data value="email"><?= $obj_user->usr_email ?></data>
+                </td>
+                <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white w-1/5">
+                    <data value="estado"><?= $obj_user->usr_estado ?></data>
+                </td>
+                <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white w-1/5">
+                    <data value="rol"><?= $obj_user->id_rol ?></data>
+                </td>
 
-                                            <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                <data value="apellido"><?= $obj_user->usr_apellido ?></data>
-                                            </td>
+                <!--botones editar y eliminar-->
+                <td class="p-4 space-x-2 whitespace-nowrap w-1/5">
 
-                                            <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                <data value="email"><?= $obj_user->usr_email ?></data>
-                                            </td>
-                                            <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                <data value="estado"><?= $obj_user->usr_estado ?></data>
-                                            </td>
-                                            <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                <data value="rol"><?= $obj_user->id_rol ?></data>
-                                            </td>
+                    <button type="button" id="updateUserButton" onclick="llenarFormularioUsuario('<?php echo $obj_user->id_usuario ?>','<?= $obj_user->usr_nombre ?>', '<?= $obj_user->usr_apellido ?>', '<?= $obj_user->usr_email ?>', '<?= $obj_user->usr_estado ?>', '<?= $obj_user->id_rol ?>');" data-drawer-target="drawer-update-user-default" data-drawer-show="drawer-update-user-default" aria-controls="drawer-update-user-default" data-drawer-placement="right" class="inline-flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-sm px-3 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <svg>
+                                <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
+                                <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path>
+                            </svg>
+                        </svg>
+                        Editar usuario
+                    </button>
 
-                                            <!--botones editar y eliminar-->
-                                            <td class="p-4 space-x-2 whitespace-nowrap">
+                    <button type="button" id="deleteUserButton" data-user-id="<?php echo $obj_user->id_usuario; ?>" data-drawer-target="drawer-delete-user-default" data-drawer-show="drawer-delete-user-default" aria-controls="drawer-delete-user-default" data-drawer-placement="right" class="inline-flex items-center px-3 py-2 text-sm font-bold text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
+                        <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
+                        </svg>
+                        Eliminar usuario
+                    </button>
 
-                                                <button type="button" id="updateUserButton" onclick="llenarFormularioUsuario('<?php echo $obj_user->id_usuario ?>','<?= $obj_user->usr_nombre ?>', '<?= $obj_user->usr_apellido ?>', '<?= $obj_user->usr_email ?>', '<?= $obj_user->usr_estado ?>', '<?= $obj_user->id_rol ?>');" data-drawer-target="drawer-update-user-default" data-drawer-show="drawer-update-user-default" aria-controls="drawer-update-user-default" data-drawer-placement="right" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-black rounded-lg bg-primary-800 hover:bg-primary-800 focus:ring-4 hover:text-white focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                                                    <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                        <svg>
-                                                            <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
-                                                            <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path>
-                                                        </svg>
-                                                    </svg>
-                                                    Editar usuario
-                                                </button>
+                </td>
+            </tr>
+        <?php endforeach; ?>
 
-                                                <button type="button" id="deleteUserButton" data-user-id="<?php echo $obj_user->id_usuario; ?>" data-drawer-target="drawer-delete-user-default" data-drawer-show="drawer-delete-user-default" aria-controls="drawer-delete-user-default" data-drawer-placement="right" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
-                                                    <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                        <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
-                                                    </svg>
-                                                    Eliminar usuario
-                                                </button>
+    </tbody>
+</table>
 
-                                            </td>
-                                        </tr>
-                                    <?php endforeach; ?>
 
-                                </tbody>
-                            </table>
                         </div>
                     </div>
                 </div>
