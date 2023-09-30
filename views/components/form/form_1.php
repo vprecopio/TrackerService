3984acf3e501906b8e5528deac0ffe935765bc3c
RoeZX <spsandoval94@gmail.com>
2023-09-30 02:30:42 -0300

    fix:correccion botones

--- views/components/form/form_1.php
+++ views/components/form/form_1.php
@@ -22,7 +22,7 @@
                 </div>
                 <!-- Modal footer -->
                 <div class="items-center p-6 border-t border-gray-200 rounded-b dark:border-gray-700">
-                    <button class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" type="submit"><?= $this->name_button ?></button>
+                    <button class="text-white bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800" type="submit"><?= $this->name_button ?></button>
                 </div>
             </form>
         </div>
