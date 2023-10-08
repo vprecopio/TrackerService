15391e3909b09dabf58e4098958e3ba5bb6b5233
Varela87 <mev_tec@yahoo.com.ar>
2023-10-08 14:21:12 -0300

    .

--- views/spareparts.php
+++ views/spareparts.php
@@ -7,15 +7,96 @@ $nombre_formulario = 'repuestos';
 
 $spareparts_controller = new SparePartsController;
 
+
+$repuestos_form = new AutomaticForm(
+    'Nuevo Repuesto', //titulo de boton principal
+    'Nuevo Repuesto', // titulo del form
+    '/spareparts/createspareparts/',
+    'POST',
+    'bg-blue-700',
+    [
+        'repuesto-nom' => [
+            'title_label' => 'Repuesto',
+            'id_name' => 'nombre',
+            'type' => 'text',
+            'height' => 6,
+            'placeholder' => 'Nombre',
+            'required' => true,
+            ],
+        
+            'repuesto-des' => [
+                'title_label' => 'Descripcion',
+                'id_name' => 'descripcion',
+                'type' => 'text',
+                'height' => 6,
+                'placeholder' => 'Descripcion',
+                'required' => true,
+            ],
+            'repuesto-empresa' => [
+                'title_label' => 'Selecciona Proveedor',
+                'id_name' => 'prov_empresa',
+                'type' => 'select',
+                'height' => 3,
+                'required' => true,
+                'options' => json_encode($spareparts_controller->ListSpareparts()),
+                'options_table' => 'prov_empresa',
+            ],
+            'repuesto-categoria' => [
+            'title_label' => 'Selecciona una Categoria',
+            'id_name' => 'descripcion_categoria',
+            'type' => 'select',
+            'height' => 3,
+            'required' => true,
+            'options' => json_encode($spareparts_controller->ListSpareparts()),
+            'options_table' => 'descripcion_categoria',
+            ],
+        
+            'repuesto-stock' => [
+                'title_label' => 'Stock',
+                'id_name' => 'stock',
+                'type' => 'text',
+                'height' => 6,
+                'placeholder' => '0',
+                'required' => true,
+            ],
+            'repuesto-costo' => [
+                'title_label' => 'Costo',
+                'id_name' => 'costo',
+                'type' => 'text',
+                'height' => 6,
+                'placeholder' => '0',
+                'required' => true,
+            ],
+            'repuesto-ganancia' => [
+                'title_label' => 'Ganancia',
+                'id_name' => 'ganancia',
+                'type' => 'text',
+                'height' => 6,
+                'placeholder' => '0',
+                'required' => true,
+            ],
+            'repuesto-stado' => [
+                'title_label' => 'Estado',
+                'id_name' => 'stado',
+                'type' => 'text',
+                'height' => 6,
+                'placeholder' => '0',
+                'required' => true,
+            ]
+
+        
+    ]
+);
+
 $categoria_form = new AutomaticForm(
-    'Insertar nueva categoria',
-    'Nueva categoria',
+    'Nueva categoria', //titulo de boton principal
+    'Nueva categoria', // titulo del form
     '/spareparts/createsparecateg/',
     'POST',
     'bg-blue-700',
     [
         'categoria-des' => [
-            'title_label' => 'Nueva categoria',
+            'title_label' => '',
             'id_name' => 'categoria_descripcion',
             'type' => 'text',
             'height' => 6,
@@ -69,7 +150,7 @@ $categoria_form = new AutomaticForm(
                     <div class="items-center justify-between block sm:flex">
 
                         <div class="flex items-center ml-auto space-x-2 sm:space-x-3">
-                            <a type="button" href="/equipment/" type="button" data-refresh="" class="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center hover:text-white text-white rounded-lg bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 sm:w-auto dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                            <a type="button" href="/spareparts/" type="button" data-refresh="" class="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center hover:text-white text-white rounded-lg bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 sm:w-auto dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
                                 <svg class="w-5 h-5 mr-2 -ml-1" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"></path>
                                 </svg>
@@ -80,6 +161,11 @@ $categoria_form = new AutomaticForm(
                             <?$categoria_form->GenerateButton() ?>
                             <?$categoria_form->GenerateForm() ?>
                             <!--FIN Agregar Categoria -->
+
+                            <!--Form agregar Repuesto -->
+                            <?$repuestos_form->GenerateButton() ?>
+                            <?$repuestos_form->GenerateForm() ?>
+                            <!--FIN Agregar Repuesto -->
 
                             
                         </div>
@@ -139,7 +225,16 @@ $categoria_form = new AutomaticForm(
                                             </td>
 
                                             <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                <data value="estado"><?= $obj_spare->estado ?></data>
+                                            <div class="flex items-center">
+                                                    <?php if ($obj_spare->estado > 0) : ?>
+                                                        <div class="h-2.5 w-2.5 rounded-full bg-green-400 mr-2"></div>
+                                                        Activo
+                                                    <?php else : ?>
+                                                        <div class="h-2.5 w-2.5 rounded-full bg-red-400 mr-2"></div>
+                                                        Inactivo
+                                                    <?php endif; ?>
+                                                </div>
+                                
                                             </td>
 
                                             <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -175,7 +270,7 @@ $categoria_form = new AutomaticForm(
                                                     Editar <?= $nombre_formulario ?>
                                                 </button>
 
-                                                <button type="button" id="deleteEquipmentButton" onclick="handleDeleteButtonClick('<?php echo $obj_spare->equipo ?>')" data-drawer-target="drawer-delete-equipment-default" data-drawer-show="drawer-delete-equipment-default" aria-controls="drawer-delete-equipment-default" data-drawer-placement="right" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
+                                                <button type="button" id="deleteSparepartsButton" onclick="handleDeleteButtonClick('<?php echo $obj_spare->equipo ?>')" data-drawer-target="drawer-delete-spareparts-default" data-drawer-show="drawer-delete-spareparts-default" aria-controls="drawer-delete-spareparts-default" data-drawer-placement="right" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
                                                     <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                         <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
                                                     </svg>
@@ -196,29 +291,31 @@ $categoria_form = new AutomaticForm(
             <!--Fin Listado equipos-->
         </entities-crud>
     </div>
-
-    <script>
-        function handleDeleteButtonClick(equiptmentId) {
-            var deleteButton = document.getElementById("deleteEquiptmentButton");
-            var deleteLink = document.querySelector("#drawer-delete-equipment-default a.text-white.bg-red-600");
-            deleteLink.href = "/equipment/delete/?id_equipo=" + equiptmentId;
-        }
+    
+   <script>
+        function handleDeleteButtonClick(sparepartsId) {
+            var deleteButton = document.getElementById("deleteSparepartsButton");
+            var deleteLink = document.querySelector("#drawer-delete-spareparts-default a.text-white.bg-red-600");
+            deleteLink.href = "/spareparts/delete/?id_repuesto=" + sparepartsId;
+        } 
     </script>
 
     <script>
         // Función para llenar los campos del formulario con los datos del formulario
       
-Equipo 	Modelo 	Marca 	Categoria 	  function llenarFormulario(equipo, modelo, marca, categoria) {
+	  function llenarFormulario(nombre, descripcion, proveedor,stock,costo,ganancia,categoria,estado) {
             // Obtener referencias a los campos del formulario
-            var equipoInput = document.getElementById("editar-equipo");
-            var modeloInput = document.getElementById("editar-modelo");
-            var marcaInput = document.getElementById("editar-marca");
-            var categoriaInput = document.getElementById("editar-categoria");
+            var nombreInput = document.getElementById("editar-nombre");
+            var descripcionInput = document.getElementById("editar-descripcion");
+           // var proveedorInput = document.getElementById("editar-marca");
+            //var categoriaInput = document.getElementById("editar-categoria");
 
             // Llenar los campos con los datos del formulario
-            equipoInput.value = equipo;
-            modeloInput.value = modelo;
-            marcaInput.value = marca;
-            categoriaInput.value = categoria;
+            repuestoInput.value = nombre;
+            descripcionInput.value = descripcion;
+            stockInput.value =stock;
+            costoInput.value = costo;
+            gananciaInput.value=ganancia;
+            estadoInput.value=estado;
         }
     </script>
