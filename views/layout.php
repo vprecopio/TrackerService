539cca07e1916240ea973eccba8eed1ff6faa233
onunezz <alegusnunez@gmail.com>
2023-10-10 13:30:56 -0300

    fix: styles fix

--- views/layout.php
+++ views/layout.php
@@ -25,14 +25,14 @@ if (isset($_SESSION["TODO"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi sitio web</title>
-    <link rel="stylesheet" href="/public/src/main.css">
+    <title>Tecno Gestión</title>
     <!-- <script src="https://cdn.tailwindcss.com"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css" rel="stylesheet" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/public/src/main.css">
 </head>
 
 <body>
