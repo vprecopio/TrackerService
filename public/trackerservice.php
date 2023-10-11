040e096e1bd8f5d3ba134f3af2b2a5e610701219
RoeZX <spsandoval94@gmail.com>
2023-10-11 07:19:59 -0300

    feature:nueva ruta externa trakerservice.php

---
+++ public/trackerservice.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tracker service</title>
+    <link rel="stylesheet" href="/public/src/main.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css" rel="stylesheet" />
+</head>
+
+<body>
+
+
+    <ol class="items-center w-full space-y-4 sm:flex sm:space-x-8 sm:space-y-0">
+        <li class="flex items-center text-blue-600 dark:text-blue-500 space-x-2.5">
+            <span class="flex items-center justify-center w-8 h-8 border border-blue-600 rounded-full shrink-0 dark:border-blue-500">
+                1
+            </span>
+            <span>
+                <h3 class="font-medium leading-tight">User info</h3>
+                <p class="text-sm">Step details here</p>
+            </span>
+        </li>
+        <li class="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5">
+            <span class="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
+                2
+            </span>
+            <span>
+                <h3 class="font-medium leading-tight">Company info</h3>
+                <p class="text-sm">Step details here</p>
+            </span>
+        </li>
+        <li class="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5">
+            <span class="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
+                3
+            </span>
+            <span>
+                <h3 class="font-medium leading-tight">Payment info</h3>
+                <p class="text-sm">Step details here</p>
+            </span>
+        </li>
+    </ol>
+
+</body>
+
+</html>
