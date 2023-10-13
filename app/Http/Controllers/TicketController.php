5d2baf9d5c6168c5f6455b5a528626b20d8d7826
RoeZX <spsandoval94@gmail.com>
2023-10-13 16:11:35 -0300

    up

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -25,10 +25,9 @@ class TicketController extends TicketModel
         $valor_ticket_total = '300';
         // Fin simulacion
 
-        if(!empty($valor_servicio) && !empty($valor_repuesto) && !empty($valor_ticket_total))
-        {
+        if (!empty($valor_servicio) && !empty($valor_repuesto) && !empty($valor_ticket_total)) {
             // cargar los atributos en TicketModel
-            $this->valor_servicios = $valor_servicio;   
+            $this->valor_servicios = $valor_servicio;
             $this->valor_repuestos = $valor_repuesto;
             $this->valor_ticket_total = $valor_ticket_total;
             // fin
@@ -36,7 +35,6 @@ class TicketController extends TicketModel
             $this->InsertVT();
             exit;
         }
-
     }
 
     public function editarvalorticket()
@@ -46,21 +44,16 @@ class TicketController extends TicketModel
         $valor_repuesto = '200';
         $valor_ticket_total = '600';
 
-        if(!empty($id_valor_ticket) && !empty($valor_servicio) && !empty($valor_repuesto) && !empty($valor_ticket_total))
-        {
+        if (!empty($id_valor_ticket) && !empty($valor_servicio) && !empty($valor_repuesto) && !empty($valor_ticket_total)) {
             $this->id_valor = $id_valor_ticket;
 
-            if(!empty($this->OneVT()))
-            {
-                $this->valor_servicios = $valor_servicio;  
+            if (!empty($this->OneVT())) {
+                $this->valor_servicios = $valor_servicio;
                 $this->valor_repuestos = $valor_repuesto;
                 $this->valor_ticket_total = $valor_ticket_total;
-        
+
                 $this->EditVT();
-               
-            }
-            else
-            {
+            } else {
                 echo 'fijate que no existe esa id';
             }
         }
@@ -69,15 +62,13 @@ class TicketController extends TicketModel
 
     public function eliminartvaloricket()
     {
-        
+
         $id_valor_ticket = '1';
-        if(!empty($id_valor_ticket))
-        {
-            $this->id_valor = $id_valor_ticket; 
+        if (!empty($id_valor_ticket)) {
+            $this->id_valor = $id_valor_ticket;
             $this->DeleteVT();
             exit;
         }
-       
     }
 
     //estados_tickets
@@ -94,16 +85,14 @@ class TicketController extends TicketModel
         $estad_ticket_descripcion = $_POST['nuevo-estado'];
         // Fin simulacion
 
-        if(!empty($estad_ticket_descripcion))
-        {
+        if (!empty($estad_ticket_descripcion)) {
             // cargar los atributos en TicketModel
-            $this->estado_ticket_descripcion = $estad_ticket_descripcion;   
+            $this->estado_ticket_descripcion = $estad_ticket_descripcion;
             // fin
 
             $this->InsertET();
             redirect('/ticket');
         }
-
     }
 
     public function editarestadoticket()
@@ -111,19 +100,14 @@ class TicketController extends TicketModel
         $id_estado_ticket = '5';
         $estad_ticket_descripcion = 'ccccc';
 
-        if(!empty($id_estado_ticket) && !empty($estad_ticket_descripcion))
-        {
+        if (!empty($id_estado_ticket) && !empty($estad_ticket_descripcion)) {
             $this->id_estado_ticket = $id_estado_ticket;
 
-            if(!empty($this->OneET()))
-            {
+            if (!empty($this->OneET())) {
                 $this->estado_ticket_descripcion = $estad_ticket_descripcion;
-        
+
                 $this->EditET();
-               
-            }
-            else
-            {
+            } else {
                 echo 'fijate que no existe esa id';
             }
         }
@@ -132,15 +116,13 @@ class TicketController extends TicketModel
 
     public function eliminarvalorticket()
     {
-        
+
         $id_estado_ticket = '6';
-        if(!empty($id_estado_ticket))
-        {
-            $this->id_estado_ticket = $id_estado_ticket; 
+        if (!empty($id_estado_ticket)) {
+            $this->id_estado_ticket = $id_estado_ticket;
             $this->DeleteET();
             exit;
         }
-       
     }
 
     //prioridades
@@ -157,16 +139,14 @@ class TicketController extends TicketModel
         $priorida_descripcion = $_POST['nueva-descripcion'];
         // Fin simulacion
 
-        if(!empty($priorida_descripcion))
-        {
+        if (!empty($priorida_descripcion)) {
             // cargar los atributos en TicketModel
-            $this->prioridad_descripcion= $priorida_descripcion;   
+            $this->prioridad_descripcion = $priorida_descripcion;
             // fin
 
             $this->InsertPrioridad();
             redirect('/ticket');
         }
-
     }
 
     public function editarprioridadticket()
@@ -174,19 +154,14 @@ class TicketController extends TicketModel
         $id_prioridad = '4';
         $priorida_descripcion = 'fijate';
 
-        if(!empty($id_prioridad) && !empty($priorida_descripcion))
-        {
-            $this->id_prioridad= $id_prioridad;
+        if (!empty($id_prioridad) && !empty($priorida_descripcion)) {
+            $this->id_prioridad = $id_prioridad;
 
-            if(!empty($this->OnePrioridad()))
-            {
+            if (!empty($this->OnePrioridad())) {
                 $this->prioridad_descripcion = $priorida_descripcion;
-        
+
                 $this->EditPrioridad();
-               
-            }
-            else
-            {
+            } else {
                 echo 'fijate que no existe esa id';
             }
         }
@@ -195,15 +170,13 @@ class TicketController extends TicketModel
 
     public function eliminarprioridadticket()
     {
-        
+
         $id_prioridad = '4';
-        if(!empty($id_prioridad))
-        {
-            $this->id_prioridad = $id_prioridad; 
+        if (!empty($id_prioridad)) {
+            $this->id_prioridad = $id_prioridad;
             $this->DeletePrioridad();
             exit;
         }
-       
     }
 
     public function listarticket()
@@ -214,34 +187,48 @@ class TicketController extends TicketModel
 
     public function delete()
     {
-        if(!isset($_GET["id_ticket"]))
-        {
-        $this->id_ticket = $_GET["id_ticket"];
-        $this->DeleteT();
-        redirect('/ticket/');
+        if (!isset($_GET["id_ticket"])) {
+            $this->id_ticket = $_GET["id_ticket"];
+            $this->DeleteT();
+            redirect('/ticket/');
         }
     }
 
     public function edit()
     {
-        echo '<pre>';
-        print_r($_POST);
-exit;
+
+        /**
+         * Array
+            (
+            [fecha_creacion] => 2023-10-01
+            [fecha_cierre] => 2023-10-02
+            [tiempo_garanti] => 2023-10-04
+            [ticket_descripcion] => fdafdsafsdf
+            [editar_valor] => 43143
+            [editar_prioridad] => alta
+            [editar_modelo] => Tone Free FN4
+            [editar_estado] => asignado
+            [editar_nombre] => Maeve
+            [editar_email] => 4321@example.com
+            [id_ticket] => 
+            [Editar] => Editar
+            )
+         */
+
 
         $this->ticket_fecha_creacion = $_POST['fecha_creacion'];
-        $this->ticket_fecha_cierre = $_POST['fecha_cierre'] ;
-        $this->ticket_tiempo_garantia = $_POST['tiempo_garanti']; 
+        $this->ticket_fecha_cierre = $_POST['fecha_cierre'];
+        $this->ticket_tiempo_garantia = $_POST['tiempo_garanti'];
         $this->ticket_descripcion = $_POST['ticket_descripcion'];
-        $id_usuario = $_POST['editar_nombre'] ;
+        $id_usuario = $_POST['editar_nombre'];
         $id_cliente = $_POST['editar_email'];
-        $this->valor_ticket_total = $_POST['editar_valor'] ;
+        $this->valor_ticket_total = $_POST['editar_valor'];
         $this->prioridad_descripcion = $_POST['editar_prioridad'];
         $id_modelo_equipo = $_POST['editar_modelo'];
-        $this->estado_ticket_descripcion = $_POST['editar_estado'] ;
-        $this->id_ticket = $_POST['id_ticket'] ;
+        $this->estado_ticket_descripcion = $_POST['editar_estado'];
+        $this->id_ticket = $_POST['id_ticket'];
         //$_POST['Editar'];
 
         exit;
     }
-
 }
