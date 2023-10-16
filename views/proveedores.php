d77db3f61ade48fcf8a1a5594d5fa3ebfce6684e
Varela87 <mev_tec@yahoo.com.ar>
2023-10-15 22:45:04 -0300

    boton eliminar proveedor

--- views/proveedores.php
+++ views/proveedores.php
@@ -297,10 +297,10 @@ $prov_crear_form = new AutomaticForm(
 
 
 
-            <!-- Delete cliente Drawer -->
+            <!-- Delete Proveedor Drawer -->
             <div id="drawer-delete-client-default" class="fixed top-0 right-0 z-40 w-full h-screen max-w-xs p-4 overflow-y-auto transition-transform bg-white dark:bg-gray-800 translate-x-full" tabindex="-1" aria-labelledby="drawer-label" aria-hidden="true">
                 <h5 id="drawer-label" class="inline-flex items-center text-sm font-semibold text-gray-500 uppercase dark:text-gray-400">
-                    Eliminar cliente
+                    Eliminar Proveedor
                 </h5>
                 <button type="button" data-drawer-dismiss="drawer-delete-client-default" aria-controls="drawer-delete-client-default" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
                     <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -312,7 +312,7 @@ $prov_crear_form = new AutomaticForm(
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                 </svg>
                 <h3 class="mb-6 text-lg text-gray-500 dark:text-gray-400">
-                    ¿Estas seguro de eliminar este cliente?
+                    ¿Estas seguro de eliminar este Proveedor?
                 </h3>
                 <a href="" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-3 py-2.5 text-center mr-2 dark:focus:ring-red-900">
                     Si, estoy seguro.
