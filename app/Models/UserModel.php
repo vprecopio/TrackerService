c176797de6f113ed554d33ecb3fa97c6c5b2ac3d
RoeZX <spsandoval94@gmail.com>
2023-10-19 00:12:24 -0300

    feature: agregar alerta al crear un usuario duplicado

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 class UserModel
 {
-    use \Database,\Sanitize;
+    use \Database, \Sanitize;
 
     protected $id_usuario;
     private $usr_nombre;
@@ -19,7 +19,8 @@ class UserModel
         $this->Connect();
     }
 
-    public function ObtenerNuevosRoles(){
+    public function ObtenerNuevosRoles()
+    {
         try {
             //consulta
             $stm = $this->pdo->prepare("SELECT * FROM `roles`");
@@ -31,86 +32,95 @@ class UserModel
 
             //en caso de que no exista ninguno devuelva 0 o false en caso contrario que devuelva "return $stm->fetchAll(\PDO::FETCH_OBJ);"
             return $stm->fetchAll(\PDO::FETCH_OBJ);
-
-        } catch (\Exception $e) {        
-
+        } catch (\Exception $e) {
         }
     }
 
-    public function ListStatus(){
-        try{
-            $stm = $this -> pdo -> prepare("SELECT DISTINCT usr_estado FROM usuarios");
+    public function ListStatus()
+    {
+        try {
+            $stm = $this->pdo->prepare("SELECT DISTINCT usr_estado FROM usuarios");
 
-            $stm -> execute();
-            return $stm->fetchAll(\PDO::FETCH_OBJ);
-        }catch(\Exception $e){
-            die($e->getMessage());
-        }
-    }
-
-
-    public function ListUser(){
-        try{
-            $stm = $this -> pdo -> prepare("SELECT * FROM usuarios JOIN roles ON usuarios.id_rol = roles.id_rol ORDER BY usuarios.id_usuario ASC");
-
-            $stm -> execute();
-            return $stm->fetchAll(\PDO::FETCH_OBJ);
-        }catch(\Exception $e){
-            die($e->getMessage());
-        }
-    }
-
-    public function OneUser(){
-        try{
-            $stm = $this -> pdo -> prepare("SELECT * FROM `usuarios` WHERE `usuarios`.`id_usuario` = :id_usuario");
-            $stm -> bindParam(':id_usuario', $this->id_usuario, \PDO::PARAM_INT);
             $stm->execute();
             return $stm->fetchAll(\PDO::FETCH_OBJ);
-        }catch(\Exception $e){
+        } catch (\Exception $e) {
             die($e->getMessage());
         }
     }
 
-    public function OneRole(){
-        try{
-            $stm = $this -> pdo -> prepare("SELECT * FROM `roles` WHERE `roles`.`rol_nombre` = :rol_nombre");
-            $stm -> bindParam(':rol_nombre', $this->rol_nombre, \PDO::PARAM_STR);
+
+    public function ListUser()
+    {
+        try {
+            $stm = $this->pdo->prepare("SELECT * FROM usuarios JOIN roles ON usuarios.id_rol = roles.id_rol ORDER BY usuarios.id_usuario ASC");
+
             $stm->execute();
             return $stm->fetchAll(\PDO::FETCH_OBJ);
-        }catch(\Exception $e){
+        } catch (\Exception $e) {
             die($e->getMessage());
         }
     }
 
-    public function CreateUser(){
-        $sql = "INSERT INTO `usuarios`(`usr_nombre`, `usr_apellido`, `usr_email`, `usr_contrasena`, `usr_estado`, `id_rol`) VALUES (:usr_nombre, :usr_apellido, :usr_email, :usr_contrasena, :usr_estado, :id_rol)";
-
-        $params=[
-            ':usr_nombre' => $this->usr_nombre,
-            ':usr_apellido' => $this->usr_apellido,
-            ':usr_email' => $this->usr_email,
-            ':usr_contrasena' => $this->usr_contrasena,
-            ':usr_estado' => $this->usr_estado,
-            ':id_rol' => $this->id_rol
-        ];
-
-        $stm = $this->pdo->prepare($sql);
-        return $stm->execute($params);
+    public function OneUser()
+    {
+        try {
+            $stm = $this->pdo->prepare("SELECT * FROM `usuarios` WHERE `usuarios`.`id_usuario` = :id_usuario");
+            $stm->bindParam(':id_usuario', $this->id_usuario, \PDO::PARAM_INT);
+            $stm->execute();
+            return $stm->fetchAll(\PDO::FETCH_OBJ);
+        } catch (\Exception $e) {
+            die($e->getMessage());
+        }
     }
 
-    public function DeleteUser(){
-        try{
+    public function OneRole()
+    {
+        try {
+            $stm = $this->pdo->prepare("SELECT * FROM `roles` WHERE `roles`.`rol_nombre` = :rol_nombre");
+            $stm->bindParam(':rol_nombre', $this->rol_nombre, \PDO::PARAM_STR);
+            $stm->execute();
+            return $stm->fetchAll(\PDO::FETCH_OBJ);
+        } catch (\Exception $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public function CreateUser()
+    {
+        try {
+            $sql = "INSERT INTO `usuarios`(`usr_nombre`, `usr_apellido`, `usr_email`, `usr_contrasena`, `usr_estado`, `id_rol`) VALUES (:usr_nombre, :usr_apellido, :usr_email, :usr_contrasena, :usr_estado, :id_rol)";
+
+            $params = [
+                ':usr_nombre' => $this->usr_nombre,
+                ':usr_apellido' => $this->usr_apellido,
+                ':usr_email' => $this->usr_email,
+                ':usr_contrasena' => $this->usr_contrasena,
+                ':usr_estado' => $this->usr_estado,
+                ':id_rol' => $this->id_rol
+            ];
+
+            $stm = $this->pdo->prepare($sql);
+            return $stm->execute($params);
+        } catch (\Exception $e) {
+            echo "<script>alert('El usuario ya existe :O')</script>";
+        }
+    }
+
+    public function DeleteUser()
+    {
+        try {
             $stm = $this->pdo->prepare("DELETE FROM `usuarios` WHERE `usuarios`.`id_usuario` = :id_usuario");
             $stm->bindParam(':id_usuario', $this->id_usuario, \PDO::PARAM_INT);
             $stm->execute();
             return true;
-        }catch(\Exception $e){
+        } catch (\Exception $e) {
             echo "<script>alert('El usuario tiene ticket asignados')</script>";
         }
     }
 
-    public function ExistUser(){
-        try{
+    public function ExistUser()
+    {
+        try {
             $sql = "SELECT COUNT(*) FROM `usuarios` WHERE `id_usuario` = :id_usuario";
             $stm = $this->pdo->prepare($sql);
             $stm->bindParam(':id_usuario', $this->id_usuario, \PDO::PARAM_INT);
@@ -119,12 +129,13 @@ class UserModel
             $result = $stm->fetchColumn();
 
             return $result > 0 ? true : false;
-        }catch(\Exception $e){
+        } catch (\Exception $e) {
             die($e->getMessage());
         }
     }
 
-    public function EditUser(){
+    public function EditUser()
+    {
         try {
             $sql = "UPDATE `usuarios` SET `usr_nombre` = :new_usr_nombre, `usr_apellido` = :new_usr_apellido, `usr_email` = :new_usr_email, `usr_estado` = :new_usr_estado, `id_rol` = :new_id_rol WHERE `id_usuario` = :id_usuario;";
 
@@ -143,13 +154,14 @@ class UserModel
         }
     }
 
-    public function ListUserRoles(){
-        try{
-            $stm = $this -> pdo -> prepare("SELECT * FROM roles");
+    public function ListUserRoles()
+    {
+        try {
+            $stm = $this->pdo->prepare("SELECT * FROM roles");
 
-            $stm -> execute();
+            $stm->execute();
             return $stm->fetchAll(\PDO::FETCH_OBJ);
-        }catch(\Exception $e){
+        } catch (\Exception $e) {
             die($e->getMessage());
         }
     }
@@ -184,7 +196,6 @@ class UserModel
 
             //en caso de que no exista ninguno devuelva 0 o false en caso contrario que devuelva "return $stm->fetchAll(\PDO::FETCH_OBJ);"
             return $stm->fetchAll(\PDO::FETCH_OBJ);
-
         } catch (\Exception $e) {
             die($e->getMessage());
         }
@@ -206,10 +217,8 @@ class UserModel
             $stm->execute();
 
             return true;
-
-        } catch (\Exception $e) {
-            die($e->getMessage());
+        } catch (\Exception) {
+            echo "<script>alert('El usuario ya existe :O')</script>";
         }
     }
-
 }
